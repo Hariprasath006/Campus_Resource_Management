0e1fac60b16e32e8094750f3151b6bdb63b6eb48
Hariprasath006 <harinataraj2005@gmail.com>
2026-05-12 09:31:35 +0530

    Update UserRepository.java

--- Backend/CampusJava/src/main/java/com/example/CampusJava/repository/UserRepository.java
+++ Backend/CampusJava/src/main/java/com/example/CampusJava/repository/UserRepository.java
@@ -10,5 +10,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
 
+    Optional<User> findByEmailIgnoreCase(String email);
+
     Optional<User> findByPhone(String phone);
 }
