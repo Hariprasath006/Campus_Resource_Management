b568065e16d0548039364cb78b357ec7e315c4b8
Hariprasath006 <harinataraj2005@gmail.com>
2026-05-12 09:47:03 +0530

    Update BookingController.java

--- Backend/CampusJava/src/main/java/com/example/CampusJava/controller/BookingController.java
+++ Backend/CampusJava/src/main/java/com/example/CampusJava/controller/BookingController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/bookings")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class BookingController {
 
     private final BookingRepository bookingRepo;
